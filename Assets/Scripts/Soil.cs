45436a049759f2224f72353e1c1343637490cdd6
FFreeze <quincymon@icloud.com>
2026-07-22 21:02:36 -0700

    Soil class GREATLY simplified

--- Assets/Scripts/Soil.cs
+++ Assets/Scripts/Soil.cs
@@ -1,35 +1,7 @@
 using UnityEngine;
 
-public class Soil : MonoBehaviour
+public class Soil
 {
-    private int _plantTimer = -1;
-    private PlantTypes.Type _plantType = PlantTypes.Type.NULL_PLANT;
-    public int PlantTimer()
-    {
-        return _plantTimer;
-    }
-    public PlantTypes.Type PlantType()
-    {
-        return _plantType;
-    }
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        _plantTimer = -1;
-        _plantType = PlantTypes.Type.NULL_PLANT;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    void OnTick()
-    {
-        if (_plantType != PlantTypes.Type.NULL_PLANT)
-        {
-            _plantTimer--;
-        }
-    }
+    public int _plantTimer = 0;
+    public PlantTypes.Type _plantType = PlantTypes.Type.NULL_PLANT;
 }
