7bc6d178410d524b6238410695b35f631982d37b
Gloomsfield <amory@acosta.boo>
2026-07-23 12:20:33 -0700

    add GetPlantAtGridPosition method

--- Assets/Scripts/Grid/Grid.cs
+++ Assets/Scripts/Grid/Grid.cs
@@ -3,11 +3,23 @@ using System.Collections.Generic;
 public class Grid {
     private List<Plant> _plants;
 
+	public int width;
+	public int height;
+
 	public void InvokeTick(PlantTypes.Type type) {
 		foreach(Plant plant in _plants) {
 			if(plant.type == type) {
 				plant.Tick();
 			}
 		}
+	}
+
+	public bool GetPlantAtGridPosition(int x, int y, out Plant result) {
+		result = null;
+
+		if(_plants.Count < y * width + x) { return false; }
+
+		result = _plants[y * width + x];
+		return true;
 	}
 }
