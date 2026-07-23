4b8834fd97539b3f34390111c985ab990e6d1373
FFreeze <quincymon@icloud.com>
2026-07-22 20:34:23 -0700

    Timer class which handles dispatching ticks

--- Assets/Scripts/TickTimer.cs
+++ Assets/Scripts/TickTimer.cs
@@ -1,36 +1,31 @@
 using System;
 using UnityEngine;
 
-public class Timer : MonoBehaviour
+public class TickTimer : MonoBehaviour
 {
-    private double _secondsPerTick;
+    public double _ticksPerSecond;
     private double _timeSinceLastTick = 0.0;
     public Action _tickUpdate;
-    public void SetTicksPerSecond(double ticksPerSecond)
-    {
-        _secondsPerTick = 1.0 / ticksPerSecond;
-    }
-    public double TicksPerSecond()
-    {
-        return 1.0 / _secondsPerTick;
-    }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        _timeSinceLastTick = 0.0; 
+        _timeSinceLastTick = 0.0;
+        _tickUpdate += OnTick;
     }
 
     // Update is called once per frame
     void Update()
     {
         _timeSinceLastTick += Time.deltaTime;
-        if (_timeSinceLastTick > _secondsPerTick)
+        double secondsPerTick = 1.0 / _ticksPerSecond;
+        if (_timeSinceLastTick >= secondsPerTick)
         {
             _tickUpdate?.Invoke();
+            _timeSinceLastTick -= secondsPerTick;
         }
     }
     private void OnTick()
     {
-        Debug.Log("Tick update!");
+        Debug.Log("Tick Timer: Tick update!");
     }
 }
