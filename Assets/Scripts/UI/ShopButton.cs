93613c73b085c3647dacbe8b3f3514045c9941c8
FFreeze <quincymon@icloud.com>
2026-07-25 15:05:33 -0700

    Plant shop icon color changes

--- Assets/Scripts/UI/ShopButton.cs
+++ Assets/Scripts/UI/ShopButton.cs
@@ -1,15 +1,23 @@
 using FMODUnity;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ShopButton : MonoBehaviour
 {
     public PlantTypes.Type plantType;
-    public TextMeshProUGUI text;
+    public TextMeshProUGUI plantName;
+    public TextMeshProUGUI plantDescription;
+    public TextMeshProUGUI plantCost;
+    public Image plantIcon;
+    public Color32 colorWhenSelected;
+    private Color32 _colorUnselected;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        text.text = $"{plantType.GetNameCapitalized()} - ${plantType.GetCost()}";
+        plantName.text = $"{plantType.GetNameCapitalized()}";
+        plantIcon.sprite = PlantTypes.TypeToSprite(plantType);
+        _colorUnselected = GetComponent<Image>().color;
     }
 
     // Update is called once per frame
@@ -20,11 +28,12 @@ public class ShopButton : MonoBehaviour
         {
             cost = (uint)(cost * 0.85);
         }
-        text.text = $"{plantType.GetNameCapitalized()} - ${cost}";
+        plantCost.text = $"{plantType.GetNameCapitalized()} - ${cost}";
     }
 
     public void SelectPlant()
     {
         Game.Instance()._player.GetComponent<Player>().SelectPlant(plantType);
+        GetComponent<Image>().color = colorWhenSelected;
     }
 }
